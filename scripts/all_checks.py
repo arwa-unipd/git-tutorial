a87a67a6bd92afff680482fe672b11b7b6f566dc
arwa.fahoud <arwa.fahoud@studenti.unipd.it>
2025-02-05 20:03:50 +0100

    Revert "add a new check to the check file"
    This reverts commit fd4281b2cbd59c86bd86dfc156f31e01cabb42db.
    reason for rollback: that we were calling a function that was never
    defined

--- scripts/all_checks.py
+++ scripts/all_checks.py
@@ -1,14 +1,10 @@
 import os
 import sys
 def check_reboot():
-
 	
 def main():
 	if check_reboot():
 		print('Pending Reboot')
-		sys.exit(1)
-	if disk_full():
-		print("disk is full")
 		sys.exit(1)
 	print('everything ok')
 	sys.exit(0)
